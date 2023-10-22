d9a2ac1040c85de6b483b3f44e3f6d18dc03c3fb
FluffyRia <fluffyriabusiness@gmail.com>
2023-10-22 19:23:54 +0400

    Script logic destribution
    Decided to split 1 script in 2 different scripts (UIObjectToggler and StartAnimationOnClick) for better reusability

---
+++ Assets/Scripts/StartAnimationOnClick.cs
@@ -0,0 +1,29 @@
+﻿using Spine;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof (Animator))]
+    public class StartAnimationOnClick : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void OnValidate()
+        {
+            _animator ??= GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            // Get the Animator component from the parent object
+            _animator = transform.parent.GetComponent<Animator>();
+        }
+
+        private void OnMouseDown()
+        {
+            // Check if the Animator component exists and is not null and play animation
+            if (_animator != null) _animator.SetTrigger("PlayAnimation");
+        }
+    }
+}
