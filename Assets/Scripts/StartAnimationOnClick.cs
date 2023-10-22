c18a87237d868b628b17b5895936ec20934021ae
FluffyRia <fluffyriabusiness@gmail.com>
2023-10-22 20:17:01 +0400

    Update scene_1
    Added animations and sounds to some static sprites and changed localization data

--- Assets/Scripts/StartAnimationOnClick.cs
+++ Assets/Scripts/StartAnimationOnClick.cs
@@ -1,29 +1,25 @@
-﻿using Spine;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
     [RequireComponent(typeof (Animator))]
     public class StartAnimationOnClick : MonoBehaviour
     {
+        [SerializeField]
         private Animator _animator;
 
         private void OnValidate()
         {
             _animator ??= GetComponent<Animator>();
         }
-
-        private void Start()
-        {
-            // Get the Animator component from the parent object
-            _animator = transform.parent.GetComponent<Animator>();
-        }
-
         private void OnMouseDown()
         {
             // Check if the Animator component exists and is not null and play animation
-            if (_animator != null) _animator.SetTrigger("PlayAnimation");
+            if (_animator != null) 
+            {
+                print("clicked");
+                _animator.SetTrigger("PlayAnimation"); 
+            }
         }
     }
 }
