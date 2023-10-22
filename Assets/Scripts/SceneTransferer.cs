b6371dae37fbe4699c409ff7df26fa351c95051c
FluffyRia <fluffyriabusiness@gmail.com>
2023-10-22 21:34:00 +0400

    Remove unnecessary components and add SceneTransferer
    Removed all examples from added assets and created script "SceneTransferer"

---
+++ Assets/Scripts/SceneTransferer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class SceneTransferer : MonoBehaviour
+    {
+        [SerializeField] private SceneAsset _scene;
+
+        public void Transfer()
+        {
+            SceneManager.LoadScene(_scene.name);
+        }
+    }
+}
